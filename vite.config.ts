4794fccec6a5bb467f6b367074a7843df183cc71
gcaso <76.giuseppe.caso@gmail.com>
2024-01-29 15:52:04 +0100

    feature: add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '@shared': fileURLToPath(new URL('./src/app/shared', import.meta.url)),
+      '@router': fileURLToPath(new URL('./src/app/router', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@domains': fileURLToPath(new URL('./src/domains', import.meta.url)),
     }
   }
 })
